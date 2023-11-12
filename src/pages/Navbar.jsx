058a15115c136d1696f937a21c008100201a13be
Juan Jose Puente Scapolatempo <l1004239@bancogalicia.com.ar>
2023-11-12 19:33:44 -0300

    navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -9,11 +9,11 @@ function NavbarComponent() {
     <Container fluid className='p-0 my-4 d-flex' >
       <Navbar expand="lg" fluid className="bg-fondo fixed-top" variant='secondary'>
         <Container fluid className='mx-0'>
-          <Navbar.Brand href="#home" as={Link} to="/Home" className='text-white ps-5'><h2 className='nav-title'>Nacho-dev</h2></Navbar.Brand>
+          <Navbar.Brand href="#home" as={Link} to="/" className='text-white ps-5'><h2 className='nav-title'>Nacho-dev</h2></Navbar.Brand>
           <Navbar.Toggle className='navbar-toggler border-white bg-white' />
           <Navbar.Collapse>
             <Nav className="ms-auto my-auto">
-              <Nav.Link as={Link} to="#" href='#home' className='text-white me-5'><h2 className='navlinks'>Home</h2></Nav.Link>
+              <Nav.Link as={Link} to="/" href='#home' className='text-white me-5'><h2 className='navlinks'>Home</h2></Nav.Link>
               <Nav.Link href="#about-me" className='text-white me-5'><h2 className='navlinks'>Sobre mí</h2></Nav.Link>
               <Nav.Link href="#habilidades" className='text-white me-5' ><h2 className='navlinks'>Habilidades</h2></Nav.Link>
               <Nav.Link href="#contacto" className='text-white me-5'><h2 className='navlinks'>Contacto</h2></Nav.Link>
